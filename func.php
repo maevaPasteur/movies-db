1e7141bcd995300c73a4005de60bb3b6b9a120c5
Maeva Pasteur <maeva.pasteur@hetic.net>
2019-12-31 14:09:31 +0100

    Feat new tables

--- func.php
+++ func.php
@@ -42,41 +42,57 @@ function template_footer() {
 EOT;
 }
 
-function template_table($title, $items, $id, $param1Title, $param1Content, $param2Title, $param2Content) {
-    $html = <<<EOT
-<div class="box-1 pb-20">
-            <h2 class="p-20">$title</h2>
+function template_table($title, $items, $type, $param1Title, $param1Content, $param2Title, $param2Content) {
+    $html = "
+<div class='box-1 pb-20' id=\"$type\">
+            <h2 class='p-20'>$title</h2>
             <hr>
-            <table class="p-20">
+            <table class='p-20'>
                 <thead>
                 <tr>
                     <th>ID</th>
                     <th>$param1Title</th>
-                    <th>$param2Title</th>
+                    ";
+    if($param2Title) {
+        $html.="<th>$param2Title</th>";
+    }
+    $html.= "
                     <th></th>
                 </tr>
                 </thead>
-                <tbody>
-EOT;
-    foreach ($items as $item) {
-        $html .= <<<EOT
-        <tr>
-                        <td>$item[$id]</td>
-                        <td>$item[$param1Content]</td>
-                        <td>$item[$param2Content]</td>
-                        <td>
-                            <a href="update.php?id=$item[$id]"><img class="icon" src="assets/images/edit.svg" alt="edit"></a>
-                            <a href="delete.php?id=$item[$id]"><img class="icon" src="assets/images/delete.svg" alt="delete"></a>
-                        </td>
-                    </tr>
-EOT;
-    }
-    $html .= <<<EOT
+                <tbody>";
+                foreach ($items as $item) {
+                    $id = $type.'ID';
+                    $html.= "
+                        <tr>
+                            <td>$item[$id]</td>
+                            ";
+                    if($type === 'poster') {
+                        $html.= "<td><img src=\"$item[$param1Content]\" alt='poster'></td>";
+                    } else {
+                        $html.= "<td>$item[$param1Content]</td>";
+                    }
+                    if($param2Content) {
+                        if($type === 'song') {
+                            $html.= "<td><a href=\"$item[$param2Content]\">$item[$param2Content]</a></td>";
+                        } else {
+                            $html.="<td>$item[$param2Content]</td>";
+                        }
+                    }
+                    $html.= "
+                            <td>
+                                <a href=\"update.php?id=$item[$id]\"><img class='icon' src='assets/images/edit.svg' alt='edit'></a>
+                                <a href=\"delete.php?id=$item[$id]\"><img class='icon' src='assets/images/delete.svg' alt='delete'></a>
+                            </td>
+                        </tr>
+                    ";
+                }
+                $html.= "
                 </tbody>
             </table>
-            <a href="create.php" class="link-1 new-client">Nouveau</a>
+            <a href='create.php' class='link-1 new-client'>Nouveau</a>
         </div>
-EOT;
+    ";
     echo $html;
 }
 ?>
